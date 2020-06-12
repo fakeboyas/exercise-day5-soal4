18341d45e717162a8ca90fd1137bfdf51ca85ac6
fakeboyas <ovoivi9@gmail.com>
2020-06-13 01:48:16 +0700

    fix Code

--- soal4.js
+++ soal4.js
@@ -1,5 +1,5 @@
-let words = prompt("Masukan Kata");
-
-const changer = (words) => words.toLowerCase();
-
-document.getElementById("demo4").innerHTML = changer(words);
+function change() {
+    let words = document.getElementById("userInput").value;
+    const changer = (words) => words.toLowerCase();
+    document.getElementById("display").innerHTML = changer(words);
+}
